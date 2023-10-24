3fe5924bcf9f47dc7a4cc4ac927e6527d31f038e
Samveg17 <81346362+Samveg17@users.noreply.github.com>
2023-10-24 21:48:16 +0530

    Update 1

---
+++ Create database.py
@@ -0,0 +1,13 @@
+import csv
+import psycopg2
+
+
+conn = psycopg2.connect(database="flask_db",  
+                        user="sequel", 
+                        password="7b80eefa",  
+                        host="localhost", port="5432")
+
+cur = conn.cursor() 
+conn.commit() 
+cur.close() 
+conn.close() 
