2fc86b6a3f2407c09680a59bdae4fec4c8ffb572
Samveg17 <samveg.bhansali17@gmail.com>
2023-10-27 02:09:12 +0530

    Update Create database.py

--- Create database.py
+++ Create database.py
@@ -1,137 +1,7 @@
 # # # 'C:/Users/asus/OneDrive/Desktop/University/Semester 7/DBMS/DBMS project'
-# # import os
-# # import csv
-# # import psycopg2
-# # import json
+import os
+# import csv
 # # import time
-
-# # # PostgreSQL connection information
-# # conn = psycopg2.connect(
-# #     dbname="postgres", user="postgres", password="asus", host="localhost"
-# # )
-# # conn.autocommit = True
-# # cur = conn.cursor()
-
-# # # Check if the database "flask_db" already exists
-# # cur.execute("SELECT 1 FROM pg_database WHERE datname='flask_db'")
-# # exists = cur.fetchone()
-
-# # # If the database doesn't exist, create it
-# # if not exists:
-# #     cur.execute("CREATE DATABASE flask_db")
-# #     print("flask_db database created successfully.")
-
-# # # Print the name of the flask_db database
-# # print("flask_db")
-
-# # # Close the cursor and connection to the default database
-# # cur.close()
-# # conn.close()
-
-# # # Reconnect to the "flask_db" database
-# # conn = psycopg2.connect(
-# #     dbname="flask_db", user="postgres", password="asus", host="localhost"
-# # )
-# # cur = conn.cursor()
-
-# # # Directory containing the CSV files
-# # csv_folder_path = 'C:/Users/asus/OneDrive/Desktop/University/Semester 7/DBMS/DBMS project'
-
-# # # Iterate through the CSV files in the folder
-# # for file_name in os.listdir(csv_folder_path):
-# #     if file_name.endswith(".csv"):
-# #         csv_file_path = os.path.join(csv_folder_path, file_name)
-
-# #         # Read the CSV file to get column names
-# #         with open(csv_file_path, "r", encoding='utf-8-sig') as file:
-# #             reader = csv.reader(file)
-# #             headers = next(reader)
-
-# #         # Modify headers to ensure the column names are valid
-# #         headers = [header.replace(" ", "_").replace("/", "_or_") for header in headers]
-
-# #         # Generate the SQL CREATE TABLE statement dynamically
-# #         table_name = os.path.splitext(file_name)[0].replace(" ", "_")  # Use the file name as the table name
-# #         columns = [f'"{header}" VARCHAR(255)' for header in headers]
-# #         create_table_query = f'CREATE TABLE "{table_name}" ({", ".join(columns)});'
-
-# #         # Execute the CREATE TABLE statement
-# #         cur.execute(create_table_query)
-
-# #         # Import data from CSV file
-# #         with open(csv_file_path, "r", encoding='utf-8-sig') as file:
-# #             next(file)  # Skip header
-# #             cur.copy_from(file, table_name, sep=",")
-
-# # # Commit the changes
-# # conn.commit()
-
-# # # Close the cursor and connection
-# # cur.close()
-# # conn.close()
-
-
-# # dataset = 'C:/Users/asus/Downloads/complete.json'
-# # def start_database():  # Create table and fill it
-# #     cur.execute(
-# #                 "CREATE TABLE IF NOT EXISTS Complete (Date DATE, State VARCHAR(30), Latitude VARCHAR(10), Longitude VARCHAR(10), deaths INT, country VARCHAR(100), geoId VARCHAR(15), popData2018 BIGINT, continent VARCHAR(50), PRIMARY KEY (fulldate, country))")
-# #     # fill_database()
-
-
-# # with open(dataset) as json_file:
-
-# #         data = json.load(json_file)
-
-# #         dataset_rows = 0
-# #         rows_inserted = 1
-# #         for p in data['records']:
-# #             dataset_rows += 1  # Count the items/records (json objects) of the dataset
-
-# #         print('This might take a while the first time...')
-# #         start = time.time()
-# #         for p in data['records']:  # For each item in the dataset
-
-# #             print(f'\rInserting to database: {rows_inserted} / {dataset_rows} rows', end="", flush=True)
-
-# #             # date = p['Date'][6:10] + '-' + p['Date'][3:5] + '-' + p['Date'][0:2]  # Convert date from dd/mm/yyyy to yyyy-mm-dd
-# #             if p['popData2019']:  # If there are population data for the current country
-# #                 sql = f"INSERT IGNORE INTO CountriesPerDay VALUES ('{p['year']}', '{p['month']}', '{p['day']}', '{date}', {p['cases']}, {p['deaths']}, '{p['countriesAndTerritories']}', '{p['geoId']}', {p['popData2019']}, '{p['continentExp']}' )"
-# #             else:  # Insert NULL on 'popData2018'
-# #                 sql = f"INSERT IGNORE INTO CountriesPerDay VALUES ('{p['year']}', '{p['month']}', '{p['day']}', '{date}', {p['cases']}, {p['deaths']}, '{p['countriesAndTerritories']}', '{p['geoId']}', NULL, '{p['continentExp']}' )"
-# #             sql = f"INSERT INTO "
-# #             mycursor.execute(sql)
-
-# #             mydb.commit()
-
-# #             rows_inserted += 1
-
-# #         end = time.time() - start
-# #         print('\n\nTime taken: ' + '{:.2f} seconds'.format(end))
-# #         print('\n')
-
-
-
-
-# from flask import Flask
-# import psycopg2
-
-# app = Flask(__name__)
-
-# # Configure the PostgreSQL connection
-# def connect_db():
-#     conn = psycopg2.connect(
-#     dbname="DBMS", user="postgres", password="asus", host="localhost")
-#     return conn.cursor
-
-# # Sample route to test the connection
-# cur = connect_db()
-# cur.execute("Select * from complete")
-# records = cur.fetchall()
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
 import psycopg2
 import json
 from datetime import datetime
@@ -220,52 +90,43 @@ def insert_json_data_to_postgres(json_data, table_name, cur):
                 )
             )
 
-
+# world population
 def create_table_from_json(json_data, table_name, cur):
-    # Drop the table if it already exists
-    cur.execute(f"DROP TABLE IF EXISTS {table_name}")
+    # formatting of the dictionary
+    for item in json_data:
+        if 'Density (P' in item:
+            density_value = item.pop('Density (P')
+            if "Km²)" in density_value:
+                item['Density (P/Km²)'] = { "Km²": density_value["Km²)"] }
 
-    # Create a new table
-    cur.execute(f'''
-        CREATE TABLE {table_name} (
+    # Save the modified data back to the JSON file
+    with open(worldpopulation, 'w') as file:
+        json.dump(json_data, file, indent=4)
 
-            country VARCHAR(255),
-            population INTEGER,
-            yearly_change VARCHAR(255),
-            net_change INTEGER,
-            density_km2 INTEGER,
-            land_area_km2 INTEGER,
-            migrants_net INTEGER,
-            fertility_rate VARCHAR(255),
-            median_age VARCHAR(255),
-            urban_population_percent VARCHAR(255),
-            world_share VARCHAR(255)
-        )
-    ''')
-
-    # Insert data into the table
+    # Ensure the structure of the JSON data and the keys match the expected format
     for data in json_data:
-        cur.execute("""
-            INSERT INTO {} (
-                country, population, yearly_change, net_change, density_km2, land_area_km2,
-                migrants_net, fertility_rate, median_age, urban_population_percent, world_share
-            ) VALUES (
-                %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
-            )""".format(table_name), (
-                data['Country (or dependency)'],
-                data['Population (2020)'],
-                data['Yearly Change'],
-                data['Net Change'],
-                data['Density (P']['Km²)'],
-                data['Land Area (Km²)'],
-                data['Migrants (net)'],
-                data['Fert. Rate'],
-                data['Med. Age'],
-                data['Urban Pop %'],
-                data['World Share']
+        if isinstance(data, dict):
+            cur.execute(f"""
+                INSERT INTO {table_name} (
+                    country_or_dependency, population_2020, yearly_change, net_change, density_per_km2, land_area_km2, migrants_net, fertility_rate, median_age, urban_pop_percentage, world_share
+                ) VALUES (
+                    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s
+                )""", (
+                    data.get('Country (or dependency)'),
+                    data.get('Population (2020)'),
+                    data.get('Yearly Change'),
+                    data.get('Net Change'),
+                    data.get('Density (P', {}).get('Km²'),
+                    data.get('Land Area (Km²)'),
+                    data.get('Migrants (net)'),
+                    data.get('Fert. Rate'),
+                    data.get('Med. Age'),
+                    data.get('Urban Pop %'),
+                    data.get('World Share')
+                )
             )
-        )
-
+        
+# test dataset
 def create_test_table_from_json(json_data, table_name, cur):
     # Drop the table if it already exists
     cur.execute(f"DROP TABLE IF EXISTS {table_name}")
@@ -273,8 +134,7 @@ def create_test_table_from_json(json_data, table_name, cur):
     # Create a new table
     cur.execute(f'''
         CREATE TABLE {table_name} (
-            id SERIAL PRIMARY KEY,
-            date DATE,
+            date VARCHAR(30),
             country_other VARCHAR(255),
             total_tests VARCHAR(255),
             population VARCHAR(255),
@@ -308,7 +168,7 @@ insert_json_data_to_postgres(covid19northamerica, 'covid19_northamerica', cur)
 insert_json_data_to_postgres(covid19southamerica, 'covid19_southamerica', cur)
 insert_json_data_to_postgres(covid19world, 'covid19_world', cur)
 create_table_from_json(worldpopulation, 'world_population', cur)
-# create_test_table_from_json(covid19tests, 'covid19_tests', cur)
+create_test_table_from_json(covid19tests, 'covid19_tests', cur)
 
 
 # Commit the changes and close the connection
