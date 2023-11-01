0aebb9846536f54bc20d8a2bd6298a9e5797c35a
Samveg17 <samveg.bhansali17@gmail.com>
2023-11-02 00:01:44 +0530

    Sent

--- app.py
+++ app.py
@@ -17,99 +17,55 @@ update_dict = {
     'World' : 'covid19_world'
 }
 
-@app.route('/dashboard2.html',  methods=['GET'] )  # Gets called when home page is requested
+@app.route('/dashboard2.html',  methods=['POST','GET'] )  # Gets called when home page is requested
 def home():
-    date_from = request.form['start_date']  # Get the given 'from date' from the form
-    date_to =   request.form['end_date']  # Get the given 'to date' from the form
-    if date_from > date_to:  # If user gives a from date that's after to date
-        # swap dates
-        temp = date_from
-        date_from = date_to
-        date_to = temp
+    if request.method == 'POST':
+        try:
+            print("nknjdfg")
 
-    if date_from and date_to:  # If both (from and to) dates were given from the user
-        # Get per day cases and deaths for the 'country' between 'date_from' and 'date_to' dates
-        covid19africa = Create_database.select_data_between_dates('covid19africa', date_from, date_to,)
-        covid19_europe = Create_database.select_data_between_dates('covid19_europe', date_from, date_to,)
-        covid19_asia = Create_database.select_data_between_dates('covid19_asia', date_from, date_to,)
-        covid19_world = Create_database.select_data_between_dates('covid19_world',date_from, date_to,)
-        covid19_oceania = Create_database.select_data_between_dates('covid19_oceania',date_from, date_to,)
-        covid19_northamerica = Create_database.select_data_between_dates('covid19_northamerica',date_from, date_to,)
-        covid19_southamerica = Create_database.select_data_between_dates('covid19_southamerica',date_from, date_to,) 
-        # country_names = Create_database.select_total_countries()
-        result = Create_database.select_data_between_dates('covid19_world', date_from, date_to,)
-        return render_template(
-            'dashboard2.html',
+            data = request.json  # Get the JSON data from the request
+            date_from = data.get('date_from')  
+            date_to = data.get('date_to')  
+            print("date_from:", date_from)
+            print("date_to:", date_to)
 
-        )
+            if date_from > date_to:  # If user gives a from date that's after to date
+                # swap dates
+                temp = date_from
+                date_from = date_to
+                date_to = temp
 
-# @app.route('/map', methods=['POST', 'GET'])  # Called when form is submitted for map.html
-# def result():
-#     date_from = request.form['start_date']  # Get the given 'from date' from the form
-#     # date_to =   request.form['date2']  # Get the given 'to date' from the form
-#     country = request.form['Country']  # Country
-#     # table_names = request.form['Continent'] 
-#     # continent = update_dict[table_names]
-#     # if date_from > date_to:  # If user gives a from date that's after to date
-#     #     # swap dates
-#     #     temp = date_from
-#     #     date_from = date_to
-#     #     date_to = temp
+            if date_from and date_to:  # If both (from and to) dates were given from the user
+                # Get per day cases and deaths for the 'country' between 'date_from' and 'date_to' dates
+                covid19_africa = Create_database.select_data_between_dates('covid19_africa', date_from, date_to,)
+                covid19_europe = Create_database.select_data_between_dates('covid19_europe', date_from, date_to,)
+                covid19_asia = Create_database.select_data_between_dates('covid19_asia', date_from, date_to,)
+                covid19_world = Create_database.select_data_between_dates('covid19_world',date_from, date_to,)
+                covid19_oceania = Create_database.select_data_between_dates('covid19_oceania',date_from, date_to,)
+                covid19_northamerica = Create_database.select_data_between_dates('covid19_northamerica',date_from, date_to,)
+                covid19_southamerica = Create_database.select_data_between_dates('covid19_southamerica',date_from, date_to,) 
+                # country_names = Create_database.select_total_countries()
+                result = Create_database.select_data_between_dates('covid19_world', date_from, date_to,)
+                return render_template(
+                    'dashboard2.html',
+                )
+        except Exception as e:
+            print(f"Error: {str(e)}")  # Print the exception message
+            return render_template('error.html', error=str(e)), 500
 
-#     # if date_from and date_to:  # If both (from and to) dates were given from the user
-#     #     # Get per day cases and deaths for the 'country' between 'date_from' and 'date_to' dates
-#     #     result = Create_database.select_data_between_dates('covid19_world', date_from, date_to,)
-#     # else:  # User selected only a country
-#     #     # Get per day cases and deaths for the 'country' for all available dates
-#     #     result = Create_database.select_total_continents(table_names)
+    elif request.method == 'GET':
+        # Your GET request handling logic here
+        # For example, you might want to initialize some variables or perform other operations specific to the GET request
+        # Then, render the 'dashboard2.html' template
+        return render_template('dashboard2.html')  # Change 'dashboard2.html' to your actual template file
 
-#     result2 = Create_database.country(country, date_from)  # Get total cases and deaths for 'country'
-#     result = Create_database.select_tests(country,date_from)
-
-#     if result2:
-#         date = result2[0][0]
-#         country = result2[0][1]
-#         confirmed = result2[0][2]
-#         deaths = result2[0][3]
-#         recovered = result2[0][4]
-#         active = result2[0][5]
-#     else:
-#         date = None
-#         country = None
-#         confirmed = None
-#         deaths = None
-#         recovered = None
-#         active = None
-
-#     if result:
-#         total_tests = result[0][2]
-#     else:
-#         total_tests = None
-#     return render_template(
-#         'map.html',
-#         # rlt4=result,
-#         rlt2=result2,
-#         # cntr_nms=country_names,
-#         # gl_cs_dths=global_cases_deaths,
-#         # d_from=date_from,
-#         # d_to=date_to,
-#         date = date,
-#         country = country,
-#         confirmed = confirmed,
-#         deaths = deaths,
-#         recovered = recovered,
-#         active = active,
-#         total_tests = total_tests,
-#     )
-
-from flask import request, render_template
+    else:
+        return 'Method Not Allowed', 405
 
 @app.route('/map.html', methods=['POST', 'GET'])
 def result():
     if request.method == 'POST':
         try:
-            print("nknj")
-
             data = request.json  # Get the JSON data from the request
             date_from = data.get('date')  # Extract the 'date' field from the JSON data
             country = data.get('country')  # Extract the 'country' field from the JSON data
@@ -197,26 +153,29 @@ def admin_as_get():
     return render_template('add_entries.html')
 
 @app.route('/add_entries.html', methods=['POST'])
-def adminlogin():
-    username = request.form['username']
-    password = request.form['password']
-    table_names = request.form['Continent'] 
-    continent = update_dict[table_names]
-    country = request.form['Country']
-    reigion = request.form['Region']
-    confirmed =  request.form['Confirmed Case']
-    deaths = request.form['Deaths']
-    recovered = request.form['Recovered']
-    active = request.form['Active Cases']
-    total_test = request.form['Total Tests']
-    population = request.form['Population']
-    tests_per_million = request.form['Test per Million']
-    test_per_person = request.form['Tests per Person']
-    date = request.form['Date']
-    if Create_database.correct_authentication(username, password):
-        # Redirect to the admin panel or handle it as needed.
-        # check the logic
-        Create_database.update(continent, date, country, reigion, confirmed, deaths, recovered, active, total_test, population, tests_per_million, test_per_person)
+def add_entries():
+    data = request.json  # Retrieve JSON data from the request
+
+    username = data['username']
+    password = data['password']
+    tableName = data['tableName']
+    continent = update_dict.get(tableName)  # Assuming update_dict is a predefined dictionary
+    country = data['country']
+    region = data['region']
+    confirmed = data['confirmed']
+    deaths = data['deaths']
+    recovered = data['recovered']
+    active = data['active']
+    totalTests = data['totalTests']
+    population = data['population']
+    testsPerMillion = data['testsPerMillion']
+    testsPerPerson = data['testsPerPerson']
+    date = data['date']
+    auth = Create_database.correct_authentication(username, password)
+    if auth==True:
+        # Redirect to the admin panel or handle it as needed
+        # Check the logic
+        Create_database.update(continent, date, country, region, confirmed, deaths, recovered, active, totalTests, population, testsPerMillion, testsPerPerson)
         return render_template('dashboard2.html')
     else:
         return render_template('add_entries.html')
